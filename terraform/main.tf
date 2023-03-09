dbe6a6f45e62f3b5f7b5a100ffe1fa81a323d4be
Colin Henderson <ColinHenderson@nc.rr.com>
2023-03-08 20:41:07 -0500

    Add cost center to Cloudant

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "ibm_cloudant" "cloudant" {
   name     = "policy-as-code-cloudant"
   location = "us-south"
   plan     = "lite"
-  # tags     = ["costcenter:001589"]
+  tags     = ["costcenter:001589"]
 
   timeouts {
     create = "15m"
